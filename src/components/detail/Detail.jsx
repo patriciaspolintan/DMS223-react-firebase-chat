e918dd40fe8a7b5d5e6372e6312686cb7df06a96
tishsp <trishasp@duck.com>
2024-05-24 11:05:56 +0800

    Deployed

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -34,7 +34,7 @@ const Detail = () => {
             <div className="user">
                 <img src={user?.avatar || "./avatar.png"} alt="" />
                 <h2>{user?.username}</h2>
-                <p>Lorem ipsum dolor sit amet.</p>
+                <p></p>
             </div>
             <div className="info">
                 <div className="option">
@@ -45,13 +45,13 @@ const Detail = () => {
                 </div>
                 <div className="option">
                     <div className="title">
-                        <span>Chat Settings</span>
+                        <span>Chat Customization</span>
                         <img src="./arrowUp.png" alt="" />
                     </div>
                 </div>
                 <div className="option">
                     <div className="title">
-                        <span>Privacy & help</span>
+                        <span>Privacy & Help</span>
                         <img src="./arrowUp.png" alt="" />
                     </div>
                 </div>
@@ -74,30 +74,30 @@ const Detail = () => {
                         <div className="photoItem">
                             <div className="photoDetail">
                                 <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                                    src="https://images.pexels.com/photos/18023668/pexels-photo-18023668/free-photo-of-close-up-of-blossoms-in-spring.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                     alt=""
                                 />
-                                <span>photo_2024_2.png</span>
+                                <span>photo.png</span>
                             </div>
                             <img src="./download.png" alt="" className="icon" />
                         </div>
                         <div className="photoItem">
                             <div className="photoDetail">
                                 <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                                    src="https://images.pexels.com/photos/1460222/pexels-photo-1460222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                     alt=""
                                 />
-                                <span>photo_2024_2.png</span>
+                                <span>image.jpg</span>
                             </div>
                             <img src="./download.png" alt="" className="icon" />
                         </div>
                         <div className="photoItem">
                             <div className="photoDetail">
                                 <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                                    src="https://images.pexels.com/photos/2836719/pexels-photo-2836719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                     alt=""
                                 />
-                                <span>photo_2024_2.png</span>
+                                <span>image.jpeg</span>
                             </div>
                             <img src="./download.png" alt="" className="icon" />
                         </div>
